2d4e5d84251e3059a8f6efbca75e92900ce4130f
developer2346 <way3671f0@gmail.com>
2023-01-04 20:43:45 +0330

    SEO and loading optimizations

--- lazy.js
+++ lazy.js
@@ -1,17 +1,27 @@
 const images = Array.from(document.querySelectorAll('.lazy'));
-        
-if ('IntersectionObserver' in window) {
-    const imageObserver = new IntersectionObserver((entries, observer) => {
+const shouldLazyload = ('onscroll' in window) && !(/(gle|ing)bot/.test(navigator.userAgent));
+
+if (('IntersectionObserver' in window) && shouldLazyload) {
+    const imageObserver = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                const image = entry.target;
+                const image = entry.target;     
 
-                image.src = image.dataset.src;
-                image.classList.add('blur-up-done')
+                image.src = image.dataset.src;   
+
+                image.addEventListener('load', () => {
+                    image.classList.add('blur-up-done')
+                }, { once: true })
+
                 imageObserver.unobserve(image);
+
             }
         });
     });
-
     images.forEach(img => imageObserver.observe(img));
+} else {
+    images.forEach(img => {
+        img.src = img.dataset.src;  
+        img.classList.remove('blur-up')
+    });
 }
